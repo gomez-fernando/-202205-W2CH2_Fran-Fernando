393e41c60bf4c5f8d4e73ef9dd7b4b3988c76e03
FranLobo <floboflores@gmail.com>
2022-05-17 18:53:50 +0200

    Upgrade push function

--- arrays.js
+++ arrays.js
@@ -1,3 +1,8 @@
 export function length(array) {
     return array.length;
 }
+export function push(array, element) {
+    //const add = (array[array.length] = element);
+    array[array.length] = element;
+    return array;
+}
--- arrays.spec.js
+++ arrays.spec.js
@@ -1,15 +1,34 @@
-import { length } from './arrays.js';
+import { length, push } from './arrays.js';
 
 describe('Given length function', () => {
+    //Function lentgh
     describe('When array have a one element', () => {
         test('should ', () => {
             //ARANGE
 
-            const array = ['pepe'];
-            const expectedResult = 1;
+            const array = ['pepe', 2];
+            const expectedResult = 2;
             //ACT
 
-            const result = array.length;
+            const result = length(array);
+
+            //ASERT
+
+            expect(result).toBe(expectedResult);
+        });
+    });
+    //Function Add
+
+    describe('When array add  one element', () => {
+        test('should ', () => {
+            //ARANGE
+
+            const array = ['pepe', 2];
+            const element = 'pepa';
+            const expectedResult = ['pepe', 2, 'pepa'];
+            //ACT
+            //console.log(expectedResult);
+            const result = push(array, element);
 
             //ASERT
 
